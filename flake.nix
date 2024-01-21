8a728275bbf7b6bc86ce8d0b9f1552e87d0c4284
blacktop <blacktop@users.noreply.github.com>
2024-01-21 13:54:42 -0700

    fixes

--- flake.nix
+++ flake.nix
@@ -1,12 +1,13 @@
 {
-  description = "My personal NUR repository";
+  description = "Blacktop's NUR repository";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   outputs = { self, nixpkgs }:
     let
       systems = [
+        "x86_64-darwin"
+        "aarch64-darwin"        
         "x86_64-linux"
         "i686-linux"
-        "x86_64-darwin"
         "aarch64-linux"
         "armv6l-linux"
         "armv7l-linux"
